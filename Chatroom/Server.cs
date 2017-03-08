06a4d93c01d363ce5beae4dfe20184b24cbf95b2
Lao Vue <Lao Vue>
2017-03-08 09:15:34 -0600

    added tcp client code to client and server classes

--- Chatroom/Server.cs
+++ Chatroom/Server.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+
 
 namespace Chatroom
 {
     class Server
     {
+        public string serverIP;
+        public Server()
+        {
+
+        }
+
+        public void AcceptClient()
+        {
+            // Retrive the Name of HOST
+            string hostName = Dns.GetHostName();
+
+            // Get the IP
+            serverIP = Dns.GetHostEntry(hostName).AddressList[1].ToString();
+
+            //Listens for client to connect
+            IPAddress ipAddress = IPAddress.Parse(serverIP);
+            TcpListener listen = new TcpListener(ipAddress, 5600);
+            Console.WriteLine("[Listenning...]");
+            listen.Start();
+            TcpClient client = listen.AcceptTcpClient();
+            Console.WriteLine("[Client connected]");
+
+            //accepts data from client
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[client.ReceiveBufferSize];
+            int data = stream.Read(buffer, 0, client.ReceiveBufferSize);
+            string ch = Encoding.Unicode.GetString(buffer, 0, data);
+            Console.WriteLine("Message Received:" + ch);
+            client.Close();
+            Console.ReadKey();
+        }
     }
 }
--- Client/Client.cs
+++ Client/Client.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
-namespace Client
+namespace Chatroom
 {
     class Client
     {
+        public string myIP;
+        public Client()
+        {
+
+        }
+
+        public void GetIpAddress()
+        {
+            // Retrive the Name of HOST
+            string hostName = Dns.GetHostName();
+            // Get the IP
+            myIP = Dns.GetHostEntry(hostName).AddressList[1].ToString();
+        }
+        public void ConnectToServer()
+        {
+            //establishes connection with server
+            TcpClient client = new TcpClient(myIP, 5600);
+            Console.WriteLine("[Try to connect to server...]");
+            //sends data to server
+            NetworkStream n = client.GetStream();
+            Console.WriteLine("[Connect]");
+            string ch = Console.ReadLine();
+            byte[] message = Encoding.Unicode.GetBytes(ch);
+            n.Write(message, 0, message.Length);
+            Console.WriteLine("--------------------sent---------------");
+            client.Close();
+            Console.ReadKey();
+        }
     }
 }
